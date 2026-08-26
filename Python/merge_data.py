478116043eda894d52a89e5aa6651fcd46ceb026
KhushbuShelake <khushbushelake5@gmail.com>
2026-08-26 17:44:15 +0530

    add python files

--- Python/merge_data.py
+++ Python/merge_data.py
@@ -1,1 +1,17 @@
+import pandas as pd
 
+military = pd.read_csv("military_cleaned.csv")
+country_info = pd.read_csv("country_info.csv")
+continents = pd.read_csv("continents2.csv")
+master_df = military.merge(
+    country_info,
+    on="country",
+    how="left"
+)
+master_df = master_df.merge(
+    continents,
+    left_on="country",
+    right_on="name",
+    how="left"
+)
+master_df.to_csv("military_master.csv", index=False)
